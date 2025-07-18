5bfd13cc89d104cdba61764d4ce157096dfbfa45
Alex Hendrix <hendrixtothemax38@gmail.com>
2025-07-18 16:19:17 -0600

    fixed for local dev

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -5,7 +5,7 @@ conda activate doom_env
 
 pip install sample-factory vizdoom ipywidgets notebook
 
-mod_file_path="$HOME/miniconda3/envs/doom_env/lib/python3.11/site-packages/sample_factory/algo/learning/learner.py"
+mod_file_path="$HOME/.conda/envs/doom_env/lib/python3.11/site-packages/sample_factory/algo/learning/learner.py"
 
 sed -i 's|checkpoint_dict = torch.load(latest_checkpoint, map_location=device)|checkpoint_dict = torch.load(latest_checkpoint, map_location=device, weights_only=False)|' $mod_file_path
 
